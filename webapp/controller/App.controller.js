5f5bba89d72c079b503e649623af8aea55042365
Arnaud Benhamdine <arnaud.benhamdine@gmail.com>
2016-03-16 18:44:49 +0100

    Add model binding in view

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,24 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], function (Controller) {
-   "use strict";
-   return Controller.extend("myApp.controller.App", {
-      onShowHello : function () {
-         // show a native JavaScript alert
-         alert("Hello World");
-      }
-   });
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+   "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+	"use strict";
+	return Controller.extend("myApp.controller.App", {
+
+		onInit: function () {
+			// set data model on view
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
+
+		onShowHello: function () {
+			MessageToast.show("Hello World");
+		}
+	});
 });
